15b083b4bcaf7e48246c070fc697246601c1ca9c
weiboyuan <weiboyuanios@163.com>
2016-11-26 14:41:00 +0800

    create file

---
+++ WBYPlaceholderTextView/WBYPlaceholderTextView.h
@@ -0,0 +1,14 @@
+//
+//  WBYPlaceholderTextView.h
+//  WBYPlaceholderProject
+//
+//  Created by weiboyuan on 2016/11/25.
+//  Copyright © 2016年 CAE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBYPlaceholderTextView : UITextView
+@property(nonatomic, copy) NSString *placeholder;
+
+@end
